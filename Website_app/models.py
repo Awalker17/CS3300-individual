6aaff8ff29f5d04664c660914df215860269b7cc
CrimsonSurfer <awalke17@uccs.edu>
2023-11-04 17:23:21 -0600

    Done with sprint1

--- Website_app/models.py
+++ Website_app/models.py
@@ -4,10 +4,16 @@ from django.urls import reverse
 # Create your models here.
 class Show(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    finished = models.BooleanField(default = False)
     season = models.CharField(max_length=3, blank = True)
     episode = models.CharField(max_length=3, blank = True)
+    language = models.CharField(max_length=200, blank = True)
+    streaming = models.CharField(max_length=200, blank = True)
+    description = models.TextField()
+    synopis = models.TextField(blank = True)
 
+
+    
     def __str__(self) -> str:
         return self.title
     def get_absolute_url(self):
