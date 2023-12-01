3249847f309ad6c9740a6128ac0f0bd2f844fba1
Awalker17 <98062325+Awalker17@users.noreply.github.com>
2023-12-01 09:38:18 -0700

    Working (#10)
    * Working in page search function
    
    * Make student model (#9)
    
    * user-detail working
    
    * All user mods added
    
    * semi working users
    
    * Sign in working
    
    * make shows working again
    
    * fixed minor bugs
    
    * documentation part 1
    
    * Permissions implemented

--- Website_app/forms.py
+++ Website_app/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
 from .models import *
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ShowForm(ModelForm):
-     class Meta:
-        model = Show
-        fields ='__all__'
+   class Meta:
+      model = Show
+      fields =["title", "finished", "season", "episode", "language", "streaming", "description", "synopis"]
+      
+class RegisterForm(UserCreationForm):
+   email = forms.EmailField(required = True)
+   class Meta:
+      model = User
+      fields =["username", "email", "password1", "password2"]
+
--- Website_app/models.py
+++ Website_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Show(models.Model):
     title = models.CharField(max_length=200)
     finished = models.BooleanField(default = False)
@@ -12,10 +14,12 @@ class Show(models.Model):
     description = models.TextField()
     synopis = models.TextField(blank = True)
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
 
     
     def __str__(self) -> str:
         return self.title
     def get_absolute_url(self):
         return reverse('show-detail', args = [str(self.id)])
-    
+
